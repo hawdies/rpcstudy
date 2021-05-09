888e311986eba1ed79f726e65350ba2da0a8d8e1
Hawdies <hawdies@foxmail.com>
2021-05-09 22:29:27 +0800

    2021-05-9

--- rpc-core/src/main/java/com/hx/remoting/transport/netty/server/NettyRpcServer.java
+++ rpc-core/src/main/java/com/hx/remoting/transport/netty/server/NettyRpcServer.java
@@ -1,14 +1,31 @@
 package com.hx.remoting.transport.netty.server;
 
+import com.hx.config.CustomShutdownHook;
 import com.hx.provider.ServiceProvider;
 import com.hx.provider.ServiceProviderImpl;
 import entity.RpcServiceProperties;
 import factory.SingletonFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import utils.concurrent.threadpool.ThreadPoolFactoryUtils;
+
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hawdies
  * @date 2021/5/6
  **/
+@Slf4j
 public class NettyRpcServer {
     public static final int PORT = 9998;
     private final ServiceProvider serviceProvider = SingletonFactory.getInstance(ServiceProviderImpl.class);
@@ -17,7 +34,42 @@ public class NettyRpcServer {
         serviceProvider.publishService(sersvice, rpcServiceProperties);
     }
 
+    @SneakyThrows
     public void start() {
         // todo 2021-05-06
+        CustomShutdownHook.getCustomShutdownHook().clearAll();
+        String host = InetAddress.getLocalHost().getHostAddress();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        DefaultEventExecutorGroup serviceHandlerGroup = new DefaultEventExecutorGroup(Runtime.getRuntime().availableProcessors() * 2,
+                ThreadPoolFactoryUtils.createThreadFactory("service-handler-group", false));
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new IdleStateHandler(30, 0, 0, TimeUnit.SECONDS));
+                            pipeline.addLast(new RpcMessageEncoder());
+                            pipeline.addLast(new RpcMessageDecoder());
+                            pipeline.addLast(serviceHandlerGroup, new NettyRpcServerHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind(host, PORT).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("occur exception when start server: ", e);
+        } finally {
+            log.error("shutdown baossGroup and workerGroup");
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
